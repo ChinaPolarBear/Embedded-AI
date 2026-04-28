c21b4c170cb8601a8aaf75ebbac92d527dd75edc
Eric-Xiong <1848199784@qq.com>
2026-04-28 11:10:47 +0100

    update

--- step1_export_trunk_matrices.py
+++ step1_export_trunk_matrices.py
@@ -20,6 +20,9 @@ After this, the FPGA model only needs:
 # python step1_export_trunk_matrices.py --ckpt hybrid_pinn_deeponet.pth --out trunk_matrices.npz
 # Shape(1,16)
 # python step1_export_trunk_matrices.py --ckpt hybrid_pinn_deeponet.pth --out trunk_matrices.npz --n_t_out 8 --time_slice uniform
+# Shape(1,16) Latent 256-->64
+# python step1_export_trunk_matrices.py --ckpt hybrid_pinn_deeponet.pth --out trunk_matrices.npz --n_t_out 8 --p_dim_out 32 --time_slice uniform
+
 import argparse
 import numpy as np
 import torch
